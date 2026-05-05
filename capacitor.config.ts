18f892a903085ced584c403c37f879c2dc4154bc
yhdp <108442936+czdyph@users.noreply.github.com>
2026-05-05 10:53:32 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sugarultra.app',
+  appName: 'Sugar Ultra',
+  webDir: 'out',
+};
+
+export default config;
